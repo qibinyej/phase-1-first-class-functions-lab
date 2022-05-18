8d396d540c0e838a1d4880732d9baeed93822567
Qibin <qbye0405@gmail.com>
2022-05-18 00:55:59 -0400

    commit tree 49fd946d7ddd11afea4dd11990b859bf0f566ad6

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(names){
+    return drivers.slice(0, -2);
+}
+
+const returnLastTwoDrivers = function(names){
+    return drivers.slice(2);
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers, 
+    ];
+
+const createFareMultiplier = i =>
+    fare => fare*i;
+
+const fareDoubler = function(i){
+    return i*2;
+}
+
+const fareTripler = function(i){
+    return i*3;
+}
+
+const selectDifferentDrivers = (i, fn) => {
+    if (fn === returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(i)
+    } else{
+        return returnLastTwoDrivers(i);
+    };
+}
+
+
+
+
+
+
+
